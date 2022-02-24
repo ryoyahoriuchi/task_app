4ae9e22bd39a96f05970478ffc02fa2d4c6d77a0
ryoya horiuchi <appleorange0720@gmail.com>
2022-02-24 10:23:42 +0900

    STEP2 デプロイ

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'タスク管理機能', type: :system do
       it '作成したタスクが表示される' do
 
         visit new_task_path
-        fill_in 'Name', with: 'test001'
-        fill_in 'Explanation', with: 'explanation001'
-        click_button 'Create Task'
+        fill_in 'タスク', with: 'test001'
+        fill_in '説明文', with: 'explanation001'
+        click_button '登録する'
         visit tasks_path
         expect(page).to have_content 'test001'
         expect(page).to have_content 'explanation001'
@@ -33,21 +33,34 @@ RSpec.describe 'タスク管理機能', type: :system do
 
       end
     end
-  end
-  describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
 
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
         visit tasks_path
-        all('table tbody tr')[0].click_link '詳細'
-        expect(page).to have_content 'test_title'
-        expect(page).to have_content 'test_explanation'
-        click_link '戻る'
-        all('table tbody tr')[1].click_link '詳細'
-        expect(page).to have_content "Factoryで作ったデフォルトの名前"
-        expect(page).to have_content 'Factoryで作ったデフォルトの説明'
+        id = all('table')
+        expect(id[0]).to have_content 'Factoryで作ったデフォルトの名前'
+        expect(id[0]).to have_content 'Factoryで作ったデフォルトの説明'
+        expect(id[1]).to have_content 'test_title'
+        expect(id[1]).to have_content 'test_explanation'
 
-       end
-     end
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+     it '該当タスクの内容が表示される' do
+
+      visit tasks_path
+      all('table tbody tr')[1].click_link '詳細'
+      expect(page).to have_content 'test_title'
+      expect(page).to have_content 'test_explanation'
+      click_link '戻る'
+      all('table tbody tr')[0].click_link '詳細'
+      expect(page).to have_content "Factoryで作ったデフォルトの名前"
+      expect(page).to have_content 'Factoryで作ったデフォルトの説明'
+
+      end
+    end
   end
 end
