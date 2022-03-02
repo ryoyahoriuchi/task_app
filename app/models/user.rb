a8d5d8c78d221159dc4921225735115dde5a5312
ryoya horiuchi <appleorange0720@gmail.com>
2022-03-02 10:52:28 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  
+  before_validation { email.downcase! }
+
+  has_secure_password
+
   has_many :tasks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "tasks#index"
   resources :tasks
+  resources :users, only: %i[ new create show ]
+  resources :sessions, only: %i[ new create destroy ]
 end
