6a2ebd9a40ceeed27d19aafcefab8aef708acf09
ryoya horiuchi <appleorange0720@gmail.com>
2022-03-04 16:48:38 +0900

    seedデータ一部修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "admin", email: "admin@mail.com", password: "password", admin: true)
+User.create!(name: "admin1", email: "admin1@mail.com", password: "password", admin: true)
 
 10.times do |n|
   name = Faker::Name.first_name
